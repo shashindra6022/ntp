ab7e2575d53da5dfea0df7d1a765136be9b9b0fa
root <root@shashiudawa5.mylabserver.com>
2017-12-08 04:19:50 +0000

    ntp

--- manifests/init.pp
+++ manifests/init.pp
@@ -1,16 +1,4 @@
-class ntp(
-  String $package_name,
-  String $package_ensure,
-  String $config_name,
-  String $config_ensure,
-  String $config_owner,
-  String $config_group,
-  String $config_mode,
-  Array[String] $servers,
-  String $service_name,
-  String $service_ensure,
-  Boolean $service_enable,
-) {
+class ntp() {
   class { '::ntp::install': }
   -> class { '::ntp::config': }
   ~> class { '::ntp::service': }
