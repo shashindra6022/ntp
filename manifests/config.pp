ab4f5346f8728028bcc2453c8a7713c588c1b1b8
root <root@shashiudawa5.mylabserver.com>
2017-12-08 04:44:19 +0000

    test paka

--- manifests/config.pp
+++ manifests/config.pp
@@ -3,7 +3,7 @@ class ntp::config(
   String $config_ensure,
   String $config_owner,
   String $config_group,
-  #String $config_mode,
+  String $config_mode,
   Array[String] $servers,
 ) { 
   file { 'ntp_config':
@@ -11,7 +11,7 @@ class ntp::config(
     path    => "/etc/${config_name}",
     owner   => $config_owner,
     group   => $config_group,
-    #mode    => $config_mode,
+    mode    => $config_mode,
     content => template("${module_name}/ntp.conf.erb")
   }
 }
