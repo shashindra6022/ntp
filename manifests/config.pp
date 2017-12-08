8b24c1a9464605be2a4c8f028ee99c78d3cee35b
root <root@shashiudawa5.mylabserver.com>
2017-12-08 06:03:20 +0000

    teting done

--- manifests/config.pp
+++ manifests/config.pp
@@ -1,4 +1,10 @@
-class ntp::config inherits ::ntp { 
+class ntp::config(
+  String $config_name   = $::ntp::config_name,
+  String $config_ensure = $::ntp::config_ensure,
+  String $config_owner  = $::ntp::config_owner,
+  String $config_group  = $::ntp::config_group,
+  String $config_mode   = $::ntp::config_mode,
+){ 
   file { 'ntp_config':
     ensure  => $config_ensure,
     path    => "/etc/${config_name}",
--- manifests/install.pp
+++ manifests/install.pp
@@ -1,4 +1,7 @@
-class ntp::install inherits ::ntp {
+class ntp::install(
+  String $package_name   = $::ntp::package_name,
+  String $package_ensure = $::ntp::package_ensure,
+){
   package { 'ntp_package':
     ensure => $package_ensure,
     name   => $package_name,
